5833bd4b9e7ff48b12c4ae905c23eff7bbc0e20b
Abhinav Jhanwar <ajhanwar97@gmail.com>
2020-04-26 21:21:20 +0530

    Update keystore

--- generate_release.sh
+++ generate_release.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o pipefail
 
-KEYSTORE=$PWD/vanadium.keystore
+KEYSTORE=$PWD/chromium.keystore
 APKSIGNER=$PWD/third_party/android_sdk/public/build-tools/29.0.2/apksigner
 BUNDLETOOL=$PWD/build/android/gyp/bundletool.py
 
@@ -15,10 +15,10 @@ rm -rf release
 mkdir release
 cd release
 
-$BUNDLETOOL build-apks --bundle ../TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium <<< $keystore_pass
+$BUNDLETOOL build-apks --bundle ../TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias chromium <<< $keystore_pass
 unzip TrichromeChrome.apks universal.apk
 mv universal.apk TrichromeChrome.apk
 
 for app in TrichromeLibrary TrichromeWebView; do
-    $APKSIGNER sign --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium --in ../$app.apk --out $app.apk <<< $keystore_pass
+    $APKSIGNER sign --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias chromium --in ../$app.apk --out $app.apk <<< $keystore_pass
 done
